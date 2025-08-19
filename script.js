6c211291118527447dfaf8a5b506ca861bc4387c
lunat-dev <H.C.lingx2@gmail.com>
2025-08-19 13:33:24 +0900

    コピペ

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // カメラ起動
 const video = document.getElementById("camera");
-navigator.mediaDevices.getUserMedia({ video: true})
-    .then(stream => {
-        console.error("カメラを起動できません:", err);
-    });
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then(stream => {
+    video.srcObject = stream;
+  })
+  .catch(err => {
+    console.error("カメラを起動できません:", err);
+  });
 
 // シャッター処理
 const captureBtn = document.getElementById("captureBtn");
@@ -12,25 +15,25 @@ const resultImg = document.getElementById("result");
 const character = document.getElementById("character");
 
 captureBtn.addEventListener("click", () => {
-    const ctx =canvas.getContext("2d");
+  const ctx = canvas.getContext("2d");
 
-    // canvasサイズをvideoと合わせる
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
+  // canvasサイズをvideoと合わせる
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
 
-    // 1. カメラ映像を描画
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+  // 1. カメラ映像を描画
+  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-    //2. キャラ画像を重ねる
-    ctx.drawImage(character, 20, 20, character.width, character.height);
+  // 2. キャラ画像を重ねる
+  ctx.drawImage(character, 20, 20, character.width, character.height);
 
-    //3. PNG画像として保存
-    const dataURL = canvas.toDataURL("image/png");
-    resultImg.src = dataURL;
+  // 3. PNG画像として保存
+  const dataURL = canvas.toDataURL("image/png");
+  resultImg.src = dataURL;
 
-    // ダウンロードリンクとして保存する場合：
-    const link = document.createElement("a");
-    link.href = dataURL;
-    link.download = "capture.png";
-    link.click();
+  // ダウンロードリンクとして保存する場合：
+  const link = document.createElement("a");
+  link.href = dataURL;
+  link.download = "capture.png";
+  link.click();
 });
